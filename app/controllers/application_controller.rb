2e3510dcd2476a36185ffebf0c403da28fff012b
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-04-01 02:06:35 -0300

    testing codeforces api with rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,40 @@
+require 'rest-client'
+require 'json'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #layout 'application', :except => :hello
   def hello
-  	render html: "cf stats"
+  	
+  	#aqui pega da API
+  	url = 'http://codeforces.com/api/user.info?handles=PedroBortolli'
+  	response = RestClient.get(url)
+
+  	#pra printar o json todo com os keys e values
+  	#render json: response
+
+  	#parse o objeto JSON
+  	parsed = JSON.parse(response)
+
+  	#pega os values da key "result" e coloca em info
+  	info = parsed['result'][0]
+
+  	#pra printar a organizacao, por exemplo:
+  	#render html: info['organization']
+
+  	#pra printar o status (se deu ok ou nao)
+  	#render html: parsed['status']
+
+  	#pra printar o lastname:
+  	#render html: info['lastName']
+
+  	firstName = info['firstName']
+  	lastName = info['lastName']
+  	organization = info['organization']
+  	rating = info['rating'].to_s
+  	s = "Pequeno teste fazendo uso da API do codeforces: ....... " + firstName + " " + lastName + ", " + organization + " ....... current rating = " + rating
+
+  	render html: s
+  	
   end
 end
