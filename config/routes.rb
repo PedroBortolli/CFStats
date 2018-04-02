aab827f267ab473791d2e66f324c64bb0dee0623
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-04-02 16:55:54 -0300

    testing controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'teste#hello'
+  root 'teste2#hello2'
 end
---
+++ test/controllers/teste_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TesteControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
