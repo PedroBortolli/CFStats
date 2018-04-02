ad2cf26f82e74b9c2a4e36585534891c21e8df21
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-04-02 18:37:06 -0300

    added more links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'about' => 'pages#about'
+  get 'api' => 'teste#hello'
   resources :posts
 end
