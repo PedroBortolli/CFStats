3dc3495a6a3e302d3c83fb9cc19fa5fce7effff2
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-04-02 21:38:52 -0300

    testing basic api page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,43 @@
+require 'rest-client'
+require 'json'
+
 class PagesController < ApplicationController
 	def index
-		
+
 	end
 
 	def about
 		@title = 'About us'
 		@content = 'Projeto de Técnicas de Programação II'
 	end
+
+	def api
+	  	#aqui pega da API
+	  	url = 'http://codeforces.com/api/user.info?handles=PedroBortolli'
+	  	response = RestClient.get(url)
+
+	  	#pra printar o json todo com os keys e values
+	  	#render json: response
+
+	  	#parse o objeto JSON
+	  	parsed = JSON.parse(response)
+
+	  	#pega os values da key "result" e coloca em info
+	  	info = parsed['result'][0]
+
+	  	#pra printar a organizacao, por exemplo:
+	  	#render html: info['organization']
+
+	  	#pra printar o status (se deu ok ou nao)
+	  	#render html: parsed['status']
+
+	  	#pra printar o lastname:
+	  	#render html: info['lastName']
+
+	  	@firstName = info['firstName']
+	  	@lastName = info['lastName']
+	  	@organization = info['organization']
+	  	@rating = info['rating'].to_s
+	  	@handle = info['handle']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'about' => 'pages#about'
-  get 'api' => 'teste#hello'
+  get 'api' => 'pages#api'
   resources :posts
 end
