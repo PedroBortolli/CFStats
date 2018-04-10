61949d31af5451dac07907e86d2ff343c0a4b61e
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-04-09 23:46:58 -0300

    now lists number of problems solved by tag for each user // code modularization, now pages_helper does everything related to data, like it should

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,47 +10,14 @@ class PagesController < ApplicationController
 	end
 
 	def result
-		input1 = params[:param1]
-		input2 = params[:param2]
-
-		if input1.to_s.empty? or input2.to_s.empty?
+		handle1 = params[:param1].to_s
+		handle2 = params[:param2].to_s
+		if handle1.empty? or handle2.to_s.empty?
 			render html: "Please provide 2 valid Codeforces handles!"
 			return
 		end
-
-		info1 = get_user_info(input1.to_s)
-		info2 = get_user_info(input2.to_s)
-
-		@handle1 = info1['handle']
-		@rating1 = info1['rating']
-		@maxRating1 = info1['maxRating']
-
-		@handle2 = info2['handle']
-		@rating2 = info2['rating']
-		@maxRating2 = info2['maxRating']
-
-		@ratingDifference = @rating1-@rating2
-		@maxRatingDifference = @maxRating1-@maxRating2
-
-		@problems1 = get_user_problems(@handle1)
-		ok1 = Array.new
-		ok2 = Array.new
-		@problems1.each do |i|
-			if i['verdict'] == 'OK'
-				ok1.push(i['problem'])
-			end
-		end
-
-		@problems2 = get_user_problems(@handle2)
-		@problems2.each do |i|
-			if i['verdict'] == 'OK'
-				ok2.push(i['problem'])
-			end
-		end
-
-		@problems = ok1 & ok2
+		@info = build_info(handle1, handle2)
 	end
-
 
 	def test
 		#### metodo para testar novas features, sera apagado futuramente! ####
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,17 +1,55 @@
 module PagesHelper
 	def get_user_info (handle)
 		base = 'http://codeforces.com/api/user.info?handles='
-		url = base + handle
+		url = base + handle.to_s
 		response = RestClient.get(url)
 		parsed = JSON.parse(response)
-		return parsed['result'][0]
+		return parsed
 	end
 
 	def get_user_problems (handle)
 		base = 'http://codeforces.com/api/user.status?handle='
-		url = base + handle
+		url = base + handle.to_s
 		response = RestClient.get(url)
 		parsed = JSON.parse(response)
 		return parsed['result']
 	end
+
+	def user_info (handle)
+		ret = get_user_info(handle)
+		return ret['result'][0]
+	end
+
+	def build_info (handle1, handle2)
+		ret = Hash.new
+		ret['handle1'] = user_info(handle1)
+		ret['handle2'] = user_info(handle2)
+		ret['ratingDifference'] = ret['handle1']['rating'] - ret['handle2']['rating']
+		ret['maxRatingDifference'] = ret['handle1']['maxRating'] - ret['handle2']['maxRating']
+
+		problems = Array.new(3)
+		problems[1] = get_user_problems(handle1)
+		problems[2] = get_user_problems(handle2)
+		ok = Array.new(3) {Array.new}
+		for i in 1..2
+			tags = Hash.new
+			problems[i].each do |problem|
+				if problem['verdict'] == 'OK'
+					ok[i].push(problem['problem'])
+				end
+				all_tags = problem['problem']['tags']
+				all_tags.each do |tag|
+					if !tags.key?(tag.to_s)
+						tags[tag] = 1
+					else
+						tags[tag] += 1
+					end
+				end
+			end
+			ret['handle'+i.to_s]['tags'] = tags
+		end
+		ret['commonProblems'] = ok[1] & ok[2]
+		return ret
+	end
+
 end
