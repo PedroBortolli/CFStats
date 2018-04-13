c573d4f8d6d7b85b793eb0718a56cdd7e0fb8b48
PedroBortolli <pedro.vitor.santos@usp.br>
2018-04-13 01:33:06 -0300

    Added contests count and min rating
    - Now counts number of contests user participated
    
    - Shows min rating of an user
    
    - Changed a few variable names to make more sense

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,18 +1,29 @@
 module PagesHelper
-	def get_user_info (handle)
-		base = 'http://codeforces.com/api/user.info?handles='
-		url = base + handle.to_s
+	def call_api (url)
 		response = RestClient.get(url)
 		parsed = JSON.parse(response)
 		return parsed
 	end
 
+	def max (a, b)
+		a > b ? a : b
+	end
+
+	def min (a, b)
+		a > b ? b : a
+	end
+
+	def get_user_info (handle)
+		base = 'http://codeforces.com/api/user.info?handles='
+		url = base + handle.to_s
+		return call_api(url)
+	end
+
 	def get_user_problems (handle)
 		base = 'http://codeforces.com/api/user.status?handle='
 		url = base + handle.to_s
-		response = RestClient.get(url)
-		parsed = JSON.parse(response)
-		return parsed['result']
+		api = call_api(url)
+		return api['result']
 	end
 
 	def user_info (handle)
@@ -20,13 +31,30 @@ module PagesHelper
 		return ret['result'][0]
 	end
 
-	def build_info (handle1, handle2)
-		ret = Hash.new
-		ret['handle1'] = user_info(handle1)
-		ret['handle2'] = user_info(handle2)
-		ret['ratingDifference'] = ret['handle1']['rating'] - ret['handle2']['rating']
-		ret['maxRatingDifference'] = ret['handle1']['maxRating'] - ret['handle2']['maxRating']
+	def contests_info (handle)
+		base = 'http://codeforces.com/api/user.rating?handle='
+		url = base + handle.to_s
+		api = call_api(url)
+		worst_rating = 1500
+		amount_contests = 0
+		api['result'].each do |contest|
+			worst_rating = min(contest['oldRating'], worst_rating)
+			worst_rating = min(contest['newRating'], worst_rating)
+			amount_contests += 1
+		end
 
+		info = Hash.new
+		info['worstRating'] = worst_rating
+		info['amountContests'] = amount_contests
+		return info
+	end
+
+	def build_info (handle1, handle2)
+		info = Hash.new
+		info['handle1'] = user_info(handle1)
+		info['handle2'] = user_info(handle2)
+		info['ratingDifference'] = info['handle1']['rating'] - info['handle2']['rating']
+		info['maxRatingDifference'] = info['handle1']['maxRating'] - info['handle2']['maxRating']
 		problems = Array.new(3)
 		problems[1] = get_user_problems(handle1)
 		problems[2] = get_user_problems(handle2)
@@ -48,12 +76,21 @@ module PagesHelper
 					end
 				end
 			end
-			ret['handle'+i.to_s]['tags'] = tags.clone
+			info['handle'+i.to_s]['tags'] = tags.clone
 		end
 		
-		ret['commonProblems'] = ok[1] & ok[2]
-		#ret = ret.sort_by {|a| a[:contestId]}
-		return ret
+		info['commonProblems'] = ok[1] & ok[2]
+		#info = info.sort_by {|a| a[:contestId]}
+
+		contests = contests_info(handle1)
+		info['handle1']['worstRating'] = contests['worstRating']
+		info['handle1']['amountContests'] = contests['amountContests']
+
+		contests = contests_info(handle2)
+		info['handle2']['worstRating'] = contests['worstRating']
+		info['handle2']['amountContests'] = contests['amountContests']
+
+		return info
 	end
 
 end
