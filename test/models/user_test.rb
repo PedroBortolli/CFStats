e7931bafa033046a9b01ba31486baeaf02806ac9
Enzo Hideki <enzo25estrela@gmail.com>
2018-06-08 18:47:20 -0300

    Adding unit test snippets

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # def setup
+  # 	@user1 = User.new()
+  # 	@user2 = User.new()
+  # end
+  
+  # test "valid user" do
+  #   assert(false)
   # end
 end
