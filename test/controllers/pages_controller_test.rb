b4cb156ec0373f8139b14b7a2171554826438c49
¨jonasarilho¨ <jonasarilho@gmail.com>
2018-06-25 22:40:42 -0300

    Now tests the /result method correctly

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,6 +4,11 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   include Api
   include DatabaseHelper
 
+  setup do
+      @p_right = {"utf8"=>"✓", "param1"=>"pedrobortolli", "param2"=>"tiago.napoli", "commit"=>"Compare!"}
+      @p_wrong = {"utf8"=>"✓", "param1"=>"trollnoexisto", "param2"=>"f7gs7dkb093ks4", "commit"=>"Compare!"}
+  end
+
   test "should get about url" do
   	get about_url
     assert_response :success
@@ -18,5 +23,18 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   	get search_url
     assert_response :success
   end
+
+  test "should get result right" do
+  	get result_url(@p_right)
+    assert_response :success
+  end
+
+  test "should get result wrong" do
+  	get result_url(@p_wrong)
+    assert_response :success
+    assert_select "body", "Please provide 2 valid Codeforces handles!"
+  end
+
+
 
 end
