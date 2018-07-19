b32a6ae2374cc3d42e98a1b3605ff524cf5ca26c
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-07-18 21:39:09 -0300

    Fixes #45

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -12,9 +12,17 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+    def show
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        set_flash_message(:success, :confirmed) if is_flashing_format?
+        respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      else
+        respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+      end
+    end
 
   # protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions',  registrations: 'users/registrations'}
+  devise_for :users, controllers: {sessions: 'users/sessions',  registrations: 'users/registrations', confirmations: 'users/confirmations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'about' => 'pages#about'
