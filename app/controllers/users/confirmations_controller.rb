6273b9f72438ffeda2aea3561299d87adeee8366
Pedro Bortolli <pedro.vitor.santos@usp.br>
2018-07-18 23:43:56 -0300

    Fixed 'didnt receive confirmation instructions?' page

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -28,9 +28,12 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # protected
 
   # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      puts("aehooooooooooooooooooooooooooo")
+      set_flash_message(:success, :send_instructions) if is_flashing_format?
+      flash[:notice] = ""
+      super(resource_name)
+    end
 
   # The path used after confirmation.
   # def after_confirmation_path_for(resource_name, resource)
